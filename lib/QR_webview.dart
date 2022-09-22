a2e640507886717e1f3e890254f7d221d88774f0
Vikramaditya <63139007+VikramadityaDev@users.noreply.github.com>
2022-09-22 12:08:24 +0530

    Updated

---
+++ lib/QR_webview.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:web_view_app/scan_page.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+
+class QRWebview extends StatefulWidget {
+
+  @override
+  State<QRWebview> createState() => _QRWebviewState();
+}
+
+class _QRWebviewState extends State<QRWebview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:WebView(
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted,
+      ) ,
+    );
+  }
+}
